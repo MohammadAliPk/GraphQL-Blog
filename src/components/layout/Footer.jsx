1665065a37887ce383e4689da11d208070e972d4
Mohammad Ali Salimian <aliparkour83@gmail.com>
2023-10-13 17:55:58 +0330

    footer added

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <Typography component="p" variant='p' bgcolor="#f7f7f7" color="primary" padding="10px" textAlign="center" mt={10}>
+        پروژه ی وبلاگ با GraphQL از محد علی سلیمیان
+            </Typography>
+        </footer>
+    );
+};
+
+export default Footer;
