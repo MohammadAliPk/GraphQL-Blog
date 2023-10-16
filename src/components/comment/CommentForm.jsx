ca827b9e9560c899467ec1575a26337f38018565
Mohammad Ali Salimian <aliparkour83@gmail.com>
2023-10-16 19:25:21 +0330

    textfield bug fixed

--- src/components/comment/CommentForm.jsx
+++ src/components/comment/CommentForm.jsx
@@ -44,13 +44,13 @@ const CommentForm = ({slug}) => {
                 <Typography component="p" variant="h6" fontWeight={700} color="primary"></Typography>
             </Grid>
             <Grid item xs={12} m={2}>
-                <TextField label="نام کاربری" variant="outlined" sx={{width: "100%"}} value={name} onChange={(e) => setName(e.target.value)}/>
+                <TextField dir="ltr" label="نام کاربری" variant="outlined" sx={{width: "100%"}} value={name} onChange={(e) => setName(e.target.value)}/>
             </Grid>
             <Grid item xs={12} m={2}>
-                <TextField label="ایمیل" variant="outlined" sx={{width:"100%"}} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <TextField dir="ltr" label="ایمیل" variant="outlined" sx={{width:"100%"}} value={email} onChange={(e) => setEmail(e.target.value)} />
             </Grid>
             <Grid item xs={12} m={2}>
-                <TextField multiline label="متن کامنت" variant="outlined" sx={{width: "100%"}} value={text} onChange={(e) => setText(e.target.value)} /> 
+                <TextField dir="ltr" multiline label="متن کامنت" variant="outlined" sx={{width: "100%"}} value={text} onChange={(e) => setText(e.target.value)} /> 
             </Grid>
             <Grid item xs={12} m={2}>
                 {loading ? <Button variant='contained' disabled>در حال ارسال</Button> : <Button variant="contained" onClick={sendHandler}>ارسال</Button>   } 
