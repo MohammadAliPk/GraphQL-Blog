6d37382559e8df2ed1ffa1f2f92b8fd45f2992ff
Mohammad Ali Salimian <aliparkour83@gmail.com>
2023-10-16 15:27:27 +0330

    Comments boxes added to project

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -8,6 +8,7 @@ import { ArrowBackRounded } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import sanitizeHtml from 'sanitize-html';
 import CommentForm from '../comment/CommentForm';
+import Comments from '../comment/Comments';
 
 const BlogPage = () => {
     const { slug } = useParams();
@@ -44,6 +45,9 @@ const BlogPage = () => {
                     </Grid>
                     <Grid item xs={12}>
                         <CommentForm slug={slug}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Comments slug={slug}/>
                     </Grid>
             </Grid>
         </Container>
--- src/components/comment/CommentForm.jsx
+++ src/components/comment/CommentForm.jsx
@@ -1,10 +1,43 @@
 import React, { useState } from 'react';
 import { Grid, TextField, Typography, Button } from '@mui/material';
+import { useMutation } from '@apollo/client';
 
-const CommentForm = () => {
+import { SEND_COMMENT } from '../../graphql/mutations';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const CommentForm = ({slug}) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [text, setText] = useState("");
+
+    const [pressed, setPressed] = useState(false);
+
+    const [sendComment, {loading , data, errors}] = useMutation(SEND_COMMENT , {variables: {
+        name,
+        email,
+        text,
+        slug
+    }});
+
+    const sendHandler = () => {
+        if(name && email && text ) {
+            sendComment();
+            setPressed(true);
+        }else {
+            toast.warn("تمام فیلد هارا پر کنید", {
+                position: "top-center"
+            })
+        }
+    };
+    if(data && pressed) {
+        toast.success("کامنت ارسال شد و منتظر تایید میباشد" , {
+            position:"top-center"
+        });
+        setPressed(false)
+    }
     return (
         <Grid container sx={{boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px" , borderRadius:4 , py:1, mt:5}}>
             <Grid item xs={12} m={2}>
@@ -20,8 +53,9 @@ const CommentForm = () => {
                 <TextField multiline label="متن کامنت" variant="outlined" sx={{width: "100%"}} value={text} onChange={(e) => setText(e.target.value)} /> 
             </Grid>
             <Grid item xs={12} m={2}>
-                <Button variant="contained">ارسال</Button>    
+                {loading ? <Button variant='contained' disabled>در حال ارسال</Button> : <Button variant="contained" onClick={sendHandler}>ارسال</Button>   } 
             </Grid>
+            <ToastContainer />
         </Grid>
     );
 };
